5b126d19fc9aa2591be9a3f576c18cb6854c2079
Kartikey Sahu <sahukartik9977@gmail.com>
2024-06-28 12:53:14 +0530

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -265,3 +265,9 @@ srtop.reveal(".experience .timeline .container", { interval: 400 });
 /* SCROLL CONTACT */
 srtop.reveal(".contact .container", { delay: 400 });
 srtop.reveal(".contact .container .form-group", { delay: 400 });
+
+
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+    
